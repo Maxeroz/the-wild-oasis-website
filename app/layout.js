62ca5c6e12f669bd6695cbb61fa14ff936d6da86
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-09-22 18:12:19 +0400

    Adding Page Metadata and Favicon

--- app/layout.js
+++ app/layout.js
@@ -4,13 +4,20 @@ import Navigation from "./_components/Navigation";
 import "@/app/_styles/globals.css";
 
 export const metadata = {
-  title: "The Wild Oasis",
+  // title: "The Wild Oasis",
+
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body className="min-h-screen bg-primary-950 text-primary-100">
         <header>
           <Logo />
           <Navigation />
