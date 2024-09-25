5bcab54101f3fba6bf3263be9ae9d8cfa41ad0c9
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-09-25 23:10:25 +0400

    Fetching and Displaying Cabin List

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,8 +1,11 @@
-export const metadata = {
-  title: "Cabins",
-};
+import CabinCard from "@/app/_components/CabinCard";
+import { getCabins } from "../_lib/data-service";
 
-export default function Cabins() {
+export default async function Page() {
+  const cabins = await getCabins();
+
+  console.log(cabins);
+
   return (
     <div>
       <h1 className="mb-5 text-4xl font-medium text-accent-400">
@@ -12,10 +15,18 @@ export default function Cabins() {
         Cozy yet luxurious cabins, located right in the heart of the Italian
         Dolomites. Imagine waking up to beautiful mountain views, spending your
         days exploring the dark forests around, or just relaxing in your private
-        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
-        home away from home. The perfect spot for a peaceful, calm vacation.
-        Welcome to paradise.
+        hot tub under the stars. Enjoy nature's beauty in your own little home
+        away from home. The perfect spot for a peaceful, calm vacation. Welcome
+        to paradise.
       </p>
+
+      {cabins.length > 0 && (
+        <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:gap-12 xl:gap-14">
+          {cabins.map((cabin) => (
+            <CabinCard cabin={cabin} key={cabin.id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
